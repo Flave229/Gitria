513db19f3dc2c178a87a1c1cf07b908d57041446
Flave <david@flave.co.uk>
2016-09-29 20:20:21 +0100

    Clicking a link on the page will now direct the user to the Github page for that specific commit

--- Gitria.Api/GitModels/GitCommit.cs
+++ Gitria.Api/GitModels/GitCommit.cs
@@ -6,13 +6,13 @@ namespace Gitria.Api.GitModels
     {
         public string repo_name { get; set; }
         public string time_ago { get; set; }
+        public string html_url { get; set; }
         public CommitInfo commit { get; set; }
         public GitAuthor author { get; set; }
     }
 
     public class CommitInfo
     {
-        public string url { get; set; }
         public CommitInfoAuthor author { get; set; }
         public string message { get; set; }
     }
