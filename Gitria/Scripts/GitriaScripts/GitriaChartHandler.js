43a8b933e764c7a34922d536cf991d5a621e89d0
Flave <david@flave.co.uk>
2016-10-15 17:15:55 +0100

    The graph has been altered so that 100% only reaches 3/4 of the way through the doughnut chart (asthetic choice). Last weeks additions and deletions also now show as a darker shade of green and red, respectivly.

--- Gitria/Scripts/GitriaScripts/GitriaChartHandler.js
+++ Gitria/Scripts/GitriaScripts/GitriaChartHandler.js
@@ -1,7 +1,9 @@
 ﻿function GenerateChartForId(repository, data)
 {
     var deletionsElement = document.getElementById(repository + "Deletions").getContext('2d');
-    debugger;
+
+    var totalDeletions = data[0].Deletions + data[1].Deletions;
+
     var myChart1 = new Chart(deletionsElement,
     {
         type: 'doughnut',
@@ -10,8 +12,8 @@
             labels: ["Deletions"],
             datasets: [
             {
-                backgroundColor: ["#F7464A", "#FFFFFF"],
-                data: [data[0].Deletions, data[1].Deletions]
+                backgroundColor: ["#F7464A", "#4C0000", "#FFFFFF"],
+                data: [(data[0].Deletions / totalDeletions) * 75, (data[1].Deletions / totalDeletions) * 75, 25]
             }]
         },
         options:
@@ -26,6 +28,8 @@
 
     var additionsElement = document.getElementById(repository + "Additions").getContext('2d');
 
+    var totalAdditions = data[0].Additions + data[1].Additions;
+
     var myChart2 = new Chart(additionsElement,
     {
         type: 'doughnut',
@@ -34,8 +38,8 @@
             labels: ["Additions"],
             datasets: [
             {
-                backgroundColor: ["#32CD32", "#FFFFFF"],
-                data: [data[0].Additions, data[1].Additions]
+                backgroundColor: ["#32CD32", "#004C00", "#FFFFFF"],
+                data: [(data[0].Additions / totalAdditions) * 75, (data[1].Additions / totalAdditions) * 75, 25]
             }]
         },
         options:
