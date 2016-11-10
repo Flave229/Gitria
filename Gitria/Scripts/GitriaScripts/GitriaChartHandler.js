e5baa1eaa929df44e2cc2c79351ae3d7df3ad01c
Flave <david@flave.co.uk>
2016-11-10 22:23:16 +0000

    Made the colors for the barchart completely opaque, as it fits the style of the page better

--- Gitria/Scripts/GitriaScripts/GitriaChartHandler.js
+++ Gitria/Scripts/GitriaScripts/GitriaChartHandler.js
@@ -118,12 +118,12 @@
                 label: 'Commits',
                 data: [sixMonthData[5].CommitCount, sixMonthData[4].CommitCount, sixMonthData[3].CommitCount, sixMonthData[2].CommitCount, sixMonthData[1].CommitCount, sixMonthData[0].CommitCount],
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.6)',
-                    'rgba(54, 162, 235, 0.6)',
-                    'rgba(255, 206, 86, 0.6)',
-                    'rgba(75, 192, 192, 0.6)',
-                    'rgba(153, 102, 255, 0.6)',
-                    'rgba(255, 159, 64, 0.6)'
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
                 ],
                 borderColor: [
                     'rgba(255,99,132,1)',
